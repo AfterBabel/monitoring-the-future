4c7b0cefb053050ea968333269f7463a9ac62b37
Cedric Warny <cedric.warny@gmail.com>
2023-02-18 16:13:52 -0500

    v1 script

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Monitoring the Future database
 
+## Pre-requisites
+
+* Install Anaconda
+* `conda activate`
+* `pip install pyreadstat`
+
 ## Steps
 
-* Get cookie
-* Download files: `./download.sh $cookie`
+* Get session id: 
+    * Log in to the UMich website in Chrome browser
+    * Open Chrome Developer Tools 
+    * Application > Storage > Cookies
+    * Filter by "JSESSIONID"
+    * Copy the first one
+* Download files: `./download.sh SESSIONID`
 * `cd files`
 * Unzip: `ls | xargs -I{} unzip {}`
--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-curl 'https://www.icpsr.umich.edu/web/NAHDAP/series/35?start=0&sort=TITLE_SORT%20asc&SERIESQ=35&ARCHIVE=NAHDAP&rows=100' \
+curl "https://www.icpsr.umich.edu/web/NAHDAP/series/35?start=0&sort=TITLE_SORT%20asc&SERIESQ=35&ARCHIVE=NAHDAP&rows=100" \
     | grep 'variables.searchResults' \
     | sed 's/variables.searchResults = \(.*\);/\1/g' \
     | tee response.json \
@@ -6,8 +6,18 @@ curl 'https://www.icpsr.umich.edu/web/NAHDAP/series/35?start=0&sort=TITLE_SORT%2
     | xargs -I{} curl 'https://www.icpsr.umich.edu/web/NAHDAP/studies/{}' \
     | grep '{"funder"' \
     | sed 's/\(.*\);/\1/g' \
-    | tee studies.json \
-    | jq '.distribution[] | select(.fileFormat=="SPSS") | .contentURL' \
-    | xargs -I{} curl {} -H 'Cookie: JSESSIONID=$1;' \
+    > studies.json
+
+mkdir files
+
+# Downloading by chunks of 10 is necessary because the curl command returns a temporary url to perform the download
+# and if you are processing a long list of temporary urls, by the time you get to the end of it, the temporary urls
+# will have expired. Processing chunks of 10 ensures the temporary urls do not get stale.
+for start in `seq 0 10 70`;
+do
+    tail -n+$start studies.json | head -n10 \
+    | jq '.distribution[] | select(.fileFormat=="Stata") | .contentURL' \
+    | xargs -I{} curl {} -H "Cookie: JSESSIONID=$1;" \
     | grep performDownload | sed 's/https/\nhttps/g' | grep ^https | sed 's/\(^https[^ <]*\)"\(.*\)/\1/g' | uniq \
     | xargs wget -P files
+done
